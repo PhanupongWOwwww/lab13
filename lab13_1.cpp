199858c8c0a7f31fdce3247cded03d6bf4460f57
PhanupongWOwwww <Zozackgt@gmail.com>
2026-01-09 11:54:46 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,34 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double A[], int N, double B[]){
+    double sum = 0.0;
+    double sumSq = 0.0;
+    double geoProd = 1.0;
+    double harmSum = 0.0;
+
+    double maxVal = A[0];
+    double minVal = A[0];
+
+    for(int i = 0; i < N; i++){
+        sum += A[i];
+        sumSq += A[i] * A[i];
+        geoProd *= A[i];
+        harmSum += 1.0 / A[i];
+
+        if(A[i] > maxVal) maxVal = A[i];
+        if(A[i] < minVal) minVal = A[i];
+    }
+
+    double mean = sum / N;
+    double stdDev = sqrt((sumSq / N) - (mean * mean));
+    double geoMean = pow(geoProd, 1.0 / N);
+    double harmMean = N / harmSum;
+
+    B[0] = mean;
+    B[1] = stdDev;
+    B[2] = geoMean;
+    B[3] = harmMean;
+    B[4] = maxVal;
+    B[5] = minVal;
+}
